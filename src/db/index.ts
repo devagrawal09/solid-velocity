3948bc50a86548a75dce6ca164b4d77ea2db26e2
Dev Agrawal <agrawadv@mail.uc.edu>
2024-06-22 15:02:50 -0400

    correct temp storage folder

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
-import { createStorage } from "unstorage";
-import fsDriver from "unstorage/drivers/fs";
+import { createStorage } from 'unstorage';
+import fsDriver from 'unstorage/drivers/fs';
 
 export const storage = createStorage({
-  driver: fsDriver({ base: "./tmp" }),
+  driver: fsDriver({ base: './.tmp' })
 });
