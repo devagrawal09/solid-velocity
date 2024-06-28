3895b7a0e67f932bba48921853479c13e39c6039
Dev Agrawal <agrawadv@mail.uc.edu>
2024-06-27 23:06:17 -0400

    using vercel kv

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,16 @@
 import { createStorage } from 'unstorage';
-import fsDriver from 'unstorage/drivers/fs';
+import vercelKVDriver from 'unstorage/drivers/vercel-kv';
+
+const kvUrl = process.env.KV_REST_API_URL;
+if (!kvUrl) throw new Error('KV_REST_API_URL is not set');
+
+const kvToken = process.env.KV_REST_API_TOKEN;
+if (!kvToken) throw new Error('KV_REST_API_URL is not set');
 
 export const storage = createStorage({
-  driver: fsDriver({ base: './.tmp' })
+  driver: vercelKVDriver({
+    url: kvUrl,
+    token: kvToken,
+    base: 'velocity'
+  })
 });
