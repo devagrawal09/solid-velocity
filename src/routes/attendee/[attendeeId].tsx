063d17869196dd9473d4a53607f93465f2070132
bhuynhdev <bhuynhdev@gmail.com>
2024-10-13 13:25:16 -0400

    Insert connection record when visit a profile url

--- src/routes/attendee/[attendeeId].tsx
+++ src/routes/attendee/[attendeeId].tsx
@@ -1,12 +1,20 @@
 import { cache, redirect, RouteDefinition } from '@solidjs/router';
+import { assertRequestAuth } from '~/auth';
+import { db } from '~/db/drizzle';
+import { connectionTable } from '~/db/schema';
 
 const scanAttendee = cache(async (attendeeId: string) => {
   'use server';
 
-  // fetch profile for current user
-  // if profile doesn't exist create it
-  // get name and avatar url from Clerk
-  // TODO: add attendeeId to connecions using current user profile id
+  // add attendeeId to connecions using current user profile id
+  const { userId } = assertRequestAuth();
+  // Find the profile id associated with this user
+  const profile = await db.query.attendeeProfiles.findFirst({
+    where: (attendee, { eq }) => eq(attendee.userId, userId)
+  });
+  if (profile) {
+    await db.insert(connectionTable).values([{ to: attendeeId, from: profile.id }]);
+  }
 
   return redirect(`/attendee`);
 }, `scanAttendee`);
