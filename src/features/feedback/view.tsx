085c83634cff8cfd73818d0d644a61579cb9ba1a
Dev Agrawal <agrawadv@mail.uc.edu>
2024-11-01 20:51:36 -0500

    speaker and attendee feedback live

--- src/features/feedback/view.tsx
+++ src/features/feedback/view.tsx
@@ -1,7 +1,6 @@
 import { createAsync } from '@solidjs/router';
 import clsx from 'clsx';
 import { For, Match, Show, Switch } from 'solid-js';
-import { AdminMode } from '../admin';
 import { getSpeakerSessionFeedback } from './api';
 import { BsEmojiFrownFill, BsEmojiNeutralFill, BsEmojiSmileFill } from 'solid-icons/bs';
 
@@ -9,54 +8,50 @@ export function ViewAttendeeFeedback() {
   const feedbackSubmissions = createAsync(() => getSpeakerSessionFeedback());
 
   return (
-    <AdminMode>
-      <div class={clsx('rounded-sm text-center text-sm my-2 py-2 bg-white bg-opacity-10')}>
-        <h2 class="text-xl font-semibold mb-2">Attendee Feedback</h2>
-        <Show
-          when={feedbackSubmissions()?.length}
-          fallback={
-            <>
-              Attendee feedback has not yet been made visible! If you think this is a mistake please
-              reach out to the Momentum organizers.
-            </>
-          }
-        >
-          <div class="text-left px-4">
-            <ul class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-2">
-              <For each={feedbackSubmissions()}>
-                {submission => (
-                  <li
-                    class={clsx('border-b border-gray-500 p-2 bg-white bg-opacity-10 text-center')}
-                  >
-                    <div class="text-center">
-                      <Switch>
-                        <Match when={submission.rating === 0}>
-                          <span class="py-4 text-2xl flex justify-center bg-red-500 bg-opacity-50 rounded m-2">
-                            <BsEmojiFrownFill />
-                          </span>
-                        </Match>
-                        <Match when={submission.rating === 1}>
-                          <span class="py-4 text-2xl flex justify-center bg-yellow-500 bg-opacity-50 rounded m-2">
-                            <BsEmojiNeutralFill />
-                          </span>
-                        </Match>
-                        <Match when={submission.rating === 2}>
-                          <span class="py-4 text-2xl flex justify-center bg-green-500 bg-opacity-50 rounded m-2">
-                            <BsEmojiSmileFill />
-                          </span>
-                        </Match>
-                      </Switch>
-                    </div>
-                    <Show when={submission.review} fallback={<em class="opacity-70">No review</em>}>
-                      <p>{submission.review}</p>
-                    </Show>
-                  </li>
-                )}
-              </For>
-            </ul>
-          </div>
-        </Show>
-      </div>
-    </AdminMode>
+    <div class={clsx('rounded-sm text-center text-sm my-2 py-2 bg-white bg-opacity-10')}>
+      <h2 class="text-xl font-semibold mb-2">Attendee Feedback</h2>
+      <Show
+        when={feedbackSubmissions()?.length}
+        fallback={
+          <>
+            Attendee feedback has not yet been made visible! If you think this is a mistake please
+            reach out to the Momentum organizers.
+          </>
+        }
+      >
+        <div class="text-left px-4">
+          <ul class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-2">
+            <For each={feedbackSubmissions()}>
+              {submission => (
+                <li class={clsx('border-b border-gray-500 p-2 bg-white bg-opacity-10 text-center')}>
+                  <div class="text-center">
+                    <Switch>
+                      <Match when={submission.rating === 0}>
+                        <span class="py-4 text-2xl flex justify-center bg-red-500 bg-opacity-50 rounded m-2">
+                          <BsEmojiFrownFill />
+                        </span>
+                      </Match>
+                      <Match when={submission.rating === 1}>
+                        <span class="py-4 text-2xl flex justify-center bg-yellow-500 bg-opacity-50 rounded m-2">
+                          <BsEmojiNeutralFill />
+                        </span>
+                      </Match>
+                      <Match when={submission.rating === 2}>
+                        <span class="py-4 text-2xl flex justify-center bg-green-500 bg-opacity-50 rounded m-2">
+                          <BsEmojiSmileFill />
+                        </span>
+                      </Match>
+                    </Switch>
+                  </div>
+                  <Show when={submission.review} fallback={<em class="opacity-70">No review</em>}>
+                    <p>{submission.review}</p>
+                  </Show>
+                </li>
+              )}
+            </For>
+          </ul>
+        </div>
+      </Show>
+    </div>
   );
 }
--- src/features/speakers/view.tsx
+++ src/features/speakers/view.tsx
@@ -8,7 +8,6 @@ import {
 } from './api';
 import { For, Show } from 'solid-js';
 import { getSessionizeData } from '../sessionize/api';
-import { AdminMode } from '../admin';
 
 export function ViewSpeakerFeedback() {
   const speakerId = createAsync(() => getRequestSpeakerFn());
@@ -25,64 +24,62 @@ export function ViewSpeakerFeedback() {
   const isSpeakerSignedUp = () => signedUpSpeakers()?.includes(speakerId() || '');
 
   return (
-    <AdminMode>
-      <Show when={isSpeakerSignedUp()}>
-        <div class={clsx('rounded-sm text-center text-sm my-2 py-2 bg-white bg-opacity-10')}>
-          <h2 class="text-xl font-semibold mb-2">Speaker Feedback</h2>
-          <Show
-            when={hydratedSubmissions().length}
-            fallback={
-              <>
-                Speaker feedback has not yet been made visible! If you think this is a mistake
-                please reach out to the Momentum organizers.
-              </>
-            }
-          >
-            <div class="text-left px-4">
-              <ul class="grid grid-cols-2">
-                <For each={hydratedSubmissions()}>
-                  {submission => (
-                    <li class="border-x border-gray-500 p-2">
-                      <p class="mb-2 text-lg">
-                        <strong>{submission.speaker.fullName}</strong>
-                      </p>
-                      <p class="mb-2">
-                        <strong>How would you rate this session?</strong>
-                        <br />
-                        {submission.data.rating}
-                      </p>
-                      <p class="mb-2">
-                        <strong>Why did you choose to attend this session?</strong>
-                        <br />
-                        {submission.data.why}
-                      </p>
-                      <p class="mb-2">
-                        <strong>What was your favorite thing about this session?</strong>
-                        <br />
-                        {submission.data.fav}
-                      </p>
-                      <p class="mb-2">
-                        <strong>
-                          What is one thing the speaker could improve the next time they present
-                          this session?
-                        </strong>
-                        <br />
-                        {submission.data.improve}
-                      </p>
-                      <p class="mb-2">
-                        <strong>Any other comments for the speaker?</strong>
-                        <br />
-                        {submission.data.comments}
-                      </p>
-                    </li>
-                  )}
-                </For>
-              </ul>
-            </div>
-          </Show>
-        </div>
-      </Show>
-    </AdminMode>
+    <Show when={isSpeakerSignedUp()}>
+      <div class={clsx('rounded-sm text-center text-sm my-2 py-2 bg-white bg-opacity-10')}>
+        <h2 class="text-xl font-semibold mb-2">Speaker Feedback</h2>
+        <Show
+          when={hydratedSubmissions().length}
+          fallback={
+            <>
+              Speaker feedback has not yet been made visible! If you think this is a mistake please
+              reach out to the Momentum organizers.
+            </>
+          }
+        >
+          <div class="text-left px-4">
+            <ul class="grid grid-cols-2">
+              <For each={hydratedSubmissions()}>
+                {submission => (
+                  <li class="border-x border-gray-500 p-2">
+                    <p class="mb-2 text-lg">
+                      <strong>{submission.speaker.fullName}</strong>
+                    </p>
+                    <p class="mb-2">
+                      <strong>How would you rate this session?</strong>
+                      <br />
+                      {submission.data.rating}
+                    </p>
+                    <p class="mb-2">
+                      <strong>Why did you choose to attend this session?</strong>
+                      <br />
+                      {submission.data.why}
+                    </p>
+                    <p class="mb-2">
+                      <strong>What was your favorite thing about this session?</strong>
+                      <br />
+                      {submission.data.fav}
+                    </p>
+                    <p class="mb-2">
+                      <strong>
+                        What is one thing the speaker could improve the next time they present this
+                        session?
+                      </strong>
+                      <br />
+                      {submission.data.improve}
+                    </p>
+                    <p class="mb-2">
+                      <strong>Any other comments for the speaker?</strong>
+                      <br />
+                      {submission.data.comments}
+                    </p>
+                  </li>
+                )}
+              </For>
+            </ul>
+          </div>
+        </Show>
+      </div>
+    </Show>
   );
 }
 
