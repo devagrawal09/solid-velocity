77087db5b8b12d6226870bc89f4b2bfddcc476d4
Dev Agrawal <agrawadv@mail.uc.edu>
2024-11-01 20:48:31 -0500

    hidden speaker feedback form

--- src/features/speakers/form.tsx
+++ src/features/speakers/form.tsx
@@ -13,6 +13,7 @@ import { getAllAssignmentsFn, getFeedbackFn, submitFeedbackFn } from './api';
 import { getSignedUpSpeakers, SpeakerFeedbackFormData, speakerFeedbackFormSchema } from './store';
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
 import { BiRegularHelpCircle } from 'solid-icons/bi';
+import { AdminMode } from '../admin';
 
 export const getRequestSpeakerOrEmptyFn = cache(async () => {
   'use server';
@@ -122,205 +123,210 @@ export function SpeakerFeedbackForm(props: { sessionId: string }) {
   const isAssigned = () => allAssignments()?.[props.sessionId]?.includes(currentSpeaker() || '');
 
   return (
-    <div
-      class={clsx(
-        'rounded-sm text-sm text-center my-2 py-2',
-        isAssigned() ? 'bg-momentum' : 'bg-white bg-opacity-10'
-      )}
-    >
-      <Tooltip>
-        <TooltipTrigger class="float-end mx-2 h-0">
-          <BiRegularHelpCircle size={25} />
-        </TooltipTrigger>
-        <TooltipContent class="w-80">
-          The Speaker Feedback Form replaces the regular feedback form for sessions that are
-          available for the Speaker-to-speaker feedback program.
-          <br />
-          {isAssigned()
-            ? 'You are assigned to this session, so you are required to submit feedback for this session.'
-            : 'You are not assigned to this session, but you are free to still attend this session and submit your feedback through this form.'}
-          <br />
-          Once submitted, you can update your response as well.
-        </TooltipContent>
-      </Tooltip>
-
-      <h2 class="text-xl font-semibold mb-2">Speaker Feedback Form</h2>
-
-      <Show
-        when={latestFeedback()}
-        fallback={
-          <Show
-            when={isAssigned()}
-            fallback={
-              <h3>
-                You have not assigned this session to yourself for speaker-to-speaker feedback. You
-                can still submit the feedback form.
-              </h3>
-            }
-          >
-            <h3>
-              You have assigned this session to yourself for speaker-to-speaker feedback. You must
-              submit the feedback form.
-            </h3>
-          </Show>
-        }
-      >
-        {feedback => (
-          <h3 class="font-bold">Last submitted: {format(feedback().timestamp, 'h:mm a, MMM d')}</h3>
+    <AdminMode>
+      <div
+        class={clsx(
+          'rounded-sm text-sm text-center my-2 py-2',
+          isAssigned() ? 'bg-momentum' : 'bg-white bg-opacity-10'
         )}
-      </Show>
-      <form
-        class="p-2 flex flex-col gap-4 text-left"
-        onSubmit={e => {
-          e.preventDefault();
-          e.stopPropagation();
-          if (submitFeedbackSubmission.pending) return;
-          form.handleSubmit();
-        }}
       >
-        <form.Field
-          name="rating"
-          validatorAdapter={zodValidator()}
-          validators={{
-            onBlur: speakerFeedbackFormSchema.shape.rating
+        <Tooltip>
+          <TooltipTrigger class="float-end mx-2 h-0">
+            <BiRegularHelpCircle size={25} />
+          </TooltipTrigger>
+          <TooltipContent class="w-80">
+            The Speaker Feedback Form replaces the regular feedback form for sessions that are
+            available for the Speaker-to-speaker feedback program.
+            <br />
+            {isAssigned()
+              ? 'You are assigned to this session, so you are required to submit feedback for this session.'
+              : 'You are not assigned to this session, but you are free to still attend this session and submit your feedback through this form.'}
+            <br />
+            Once submitted, you can update your response as well.
+          </TooltipContent>
+        </Tooltip>
+
+        <h2 class="text-xl font-semibold mb-2">Speaker Feedback Form</h2>
+
+        <Show
+          when={latestFeedback()}
+          fallback={
+            <Show
+              when={isAssigned()}
+              fallback={
+                <h3>
+                  You have not assigned this session to yourself for speaker-to-speaker feedback.
+                  You can still submit the feedback form.
+                </h3>
+              }
+            >
+              <h3>
+                You have assigned this session to yourself for speaker-to-speaker feedback. You must
+                submit the feedback form.
+              </h3>
+            </Show>
+          }
+        >
+          {feedback => (
+            <h3 class="font-bold">
+              Last submitted: {format(feedback().timestamp, 'h:mm a, MMM d')}
+            </h3>
+          )}
+        </Show>
+        <form
+          class="p-2 flex flex-col gap-4 text-left"
+          onSubmit={e => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (submitFeedbackSubmission.pending) return;
+            form.handleSubmit();
           }}
-          children={field => (
-            <div class="flex flex-col gap-2">
-              How would you rate this session?
-              <div class="grid grid-cols-5 gap-2">
-                <For each={ratings}>
-                  {rating => (
-                    <div>
-                      <input
-                        class="appearance-none peer h-0"
-                        type="radio"
-                        name={field().name}
-                        value={field().state.value || ''}
-                        checked={field().state.value === parseInt(rating.value)}
-                        onChange={e => field().handleChange(parseInt(rating.value) as any)}
-                        id={rating.value}
-                      />
-                      <label
-                        for={rating.value}
-                        class={`text-center flex flex-col justify-center text-xl -mt-4 h-10 bg-opacity-50 rounded disabled:outline peer-checked:bg-opacity-80 peer-checked:border-2 peer-hover:bg-opacity-100 peer-focus:bg-opacity-100 ${rating.color}`}
-                      >
-                        {rating.value}
-                      </label>
-                    </div>
-                  )}
+        >
+          <form.Field
+            name="rating"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: speakerFeedbackFormSchema.shape.rating
+            }}
+            children={field => (
+              <div class="flex flex-col gap-2">
+                How would you rate this session?
+                <div class="grid grid-cols-5 gap-2">
+                  <For each={ratings}>
+                    {rating => (
+                      <div>
+                        <input
+                          class="appearance-none peer h-0"
+                          type="radio"
+                          name={field().name}
+                          value={field().state.value || ''}
+                          checked={field().state.value === parseInt(rating.value)}
+                          onChange={e => field().handleChange(parseInt(rating.value) as any)}
+                          id={rating.value}
+                        />
+                        <label
+                          for={rating.value}
+                          class={`text-center flex flex-col justify-center text-xl -mt-4 h-10 bg-opacity-50 rounded disabled:outline peer-checked:bg-opacity-80 peer-checked:border-2 peer-hover:bg-opacity-100 peer-focus:bg-opacity-100 ${rating.color}`}
+                        >
+                          {rating.value}
+                        </label>
+                      </div>
+                    )}
+                  </For>
+                </div>
+                <For each={field().state.meta.errors}>
+                  {error => <em class="text-red-400">{error}</em>}
                 </For>
               </div>
-              <For each={field().state.meta.errors}>
-                {error => <em class="text-red-400">{error}</em>}
-              </For>
-            </div>
-          )}
-        />
-        <form.Field
-          name="why"
-          validatorAdapter={zodValidator()}
-          validators={{
-            onBlur: speakerFeedbackFormSchema.shape.why
-          }}
-          children={field => (
-            <div class="flex flex-col gap-2">
-              <label for={field().name}>Why did you choose to attend this session?</label>
-              <textarea
-                id={field().name}
-                value={field().state.value || ''}
-                onBlur={field().handleBlur}
-                onInput={e => field().handleChange(e.target.value)}
-                class="text-black px-3 py-2 rounded"
-              />
-              <For each={field().state.meta.errors}>
-                {error => <em class="text-red-400">{error}</em>}
-              </For>
-            </div>
-          )}
-        />
-        <form.Field
-          name="fav"
-          validatorAdapter={zodValidator()}
-          validators={{
-            onBlur: speakerFeedbackFormSchema.shape.fav
-          }}
-          children={field => (
-            <div class="flex flex-col gap-2">
-              <label for={field().name}>
-                What was your favorite thing about this session?
-                <br />
-                <span class="text-xs">
-                  What did you learn from this session? Is there anything it made you think about?
-                </span>
-              </label>
-              <textarea
-                id={field().name}
-                value={field().state.value || ''}
-                onBlur={field().handleBlur}
-                onInput={e => field().handleChange(e.target.value)}
-                class="text-black px-3 py-2 rounded"
-              />
-              <For each={field().state.meta.errors}>
-                {error => <em class="text-red-400">{error}</em>}
-              </For>
-            </div>
-          )}
-        />
-        <form.Field
-          name="improve"
-          validatorAdapter={zodValidator()}
-          validators={{
-            onBlur: speakerFeedbackFormSchema.shape.improve
-          }}
-          children={field => (
-            <div class="flex flex-col gap-2">
-              <label for={field().name}>
-                What is one thing the speaker could improve the next time they present this session?
-              </label>
-              <textarea
-                id={field().name}
-                value={field().state.value || ''}
-                onBlur={field().handleBlur}
-                onInput={e => field().handleChange(e.target.value)}
-                class="text-black px-3 py-2 rounded"
-              />
-              <For each={field().state.meta.errors}>
-                {error => <em class="text-red-400">{error}</em>}
-              </For>
-            </div>
-          )}
-        />
-        <form.Field
-          name="comments"
-          validatorAdapter={zodValidator()}
-          validators={{
-            onBlur: speakerFeedbackFormSchema.shape.comments
-          }}
-          children={field => (
-            <div class="flex flex-col gap-2">
-              <label for={field().name}>Any other comments for the speaker?</label>
-              <textarea
-                id={field().name}
-                value={field().state.value || ''}
-                onBlur={field().handleBlur}
-                onInput={e => field().handleChange(e.target.value)}
-                class="text-black px-3 py-2 rounded"
-              />
-              <For each={field().state.meta.errors}>
-                {error => <em class="text-red-400">{error}</em>}
-              </For>
-            </div>
-          )}
-        />
+            )}
+          />
+          <form.Field
+            name="why"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: speakerFeedbackFormSchema.shape.why
+            }}
+            children={field => (
+              <div class="flex flex-col gap-2">
+                <label for={field().name}>Why did you choose to attend this session?</label>
+                <textarea
+                  id={field().name}
+                  value={field().state.value || ''}
+                  onBlur={field().handleBlur}
+                  onInput={e => field().handleChange(e.target.value)}
+                  class="text-black px-3 py-2 rounded"
+                />
+                <For each={field().state.meta.errors}>
+                  {error => <em class="text-red-400">{error}</em>}
+                </For>
+              </div>
+            )}
+          />
+          <form.Field
+            name="fav"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: speakerFeedbackFormSchema.shape.fav
+            }}
+            children={field => (
+              <div class="flex flex-col gap-2">
+                <label for={field().name}>
+                  What was your favorite thing about this session?
+                  <br />
+                  <span class="text-xs">
+                    What did you learn from this session? Is there anything it made you think about?
+                  </span>
+                </label>
+                <textarea
+                  id={field().name}
+                  value={field().state.value || ''}
+                  onBlur={field().handleBlur}
+                  onInput={e => field().handleChange(e.target.value)}
+                  class="text-black px-3 py-2 rounded"
+                />
+                <For each={field().state.meta.errors}>
+                  {error => <em class="text-red-400">{error}</em>}
+                </For>
+              </div>
+            )}
+          />
+          <form.Field
+            name="improve"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: speakerFeedbackFormSchema.shape.improve
+            }}
+            children={field => (
+              <div class="flex flex-col gap-2">
+                <label for={field().name}>
+                  What is one thing the speaker could improve the next time they present this
+                  session?
+                </label>
+                <textarea
+                  id={field().name}
+                  value={field().state.value || ''}
+                  onBlur={field().handleBlur}
+                  onInput={e => field().handleChange(e.target.value)}
+                  class="text-black px-3 py-2 rounded"
+                />
+                <For each={field().state.meta.errors}>
+                  {error => <em class="text-red-400">{error}</em>}
+                </For>
+              </div>
+            )}
+          />
+          <form.Field
+            name="comments"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: speakerFeedbackFormSchema.shape.comments
+            }}
+            children={field => (
+              <div class="flex flex-col gap-2">
+                <label for={field().name}>Any other comments for the speaker?</label>
+                <textarea
+                  id={field().name}
+                  value={field().state.value || ''}
+                  onBlur={field().handleBlur}
+                  onInput={e => field().handleChange(e.target.value)}
+                  class="text-black px-3 py-2 rounded"
+                />
+                <For each={field().state.meta.errors}>
+                  {error => <em class="text-red-400">{error}</em>}
+                </For>
+              </div>
+            )}
+          />
 
-        <Button
-          type="submit"
-          class="bg-momentum rounded m-auto py-2 px-4"
-          disabled={submitFeedbackSubmission.pending}
-        >
-          Submit
-        </Button>
-      </form>
-    </div>
+          <Button
+            type="submit"
+            class="bg-momentum rounded m-auto py-2 px-4"
+            disabled={submitFeedbackSubmission.pending}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+    </AdminMode>
   );
 }
